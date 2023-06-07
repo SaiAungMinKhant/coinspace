7950d838b2b6a1fb64550aaf268559cd94e4770c
Sai Aung Min Khant <saiaungminkhant20@gmail.com>
2023-06-07 19:55:25 +0630

    commit

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './scss/main.scss'
+import Table from './components/Table'
+import SearchBox from './components/Searchbox'
 
 function App() {
 
   return (
     <>
-
+      <SearchBox />
+      <Table />
     </>
   )
 }
