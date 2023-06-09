8f6e33168c90d98008b32039882221b9962baa2c
Sai Aung Min Khant <saiaungminkhant20@gmail.com>
2023-06-09 19:16:07 +0630

    commit

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -12,11 +12,11 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('1', 159, 6.0, 24, 4.0),
+  createData('2', 237, 9.0, 37, 4.3),
+  createData('4', 262, 16.0, 24, 6.0),
+  createData('5', 305, 3.7, 67, 4.3),
+  createData('6', 356, 16.0, 49, 3.9),
 ];
 
 export default function BasicTable() {
@@ -25,11 +25,10 @@ export default function BasicTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>#</TableCell>
+            <TableCell align="right">Coin</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">24hr</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
