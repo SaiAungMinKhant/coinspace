c168e1bc423b5c4e49b64808bccc4afe5cbf6763
Sai Aung Min Khant <saiaungminkhant20@gmail.com>
2023-07-19 00:47:18 +0630

    samk

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Table from './components/Table'
+
+function App() {
+
+  return (
+    <div className='font-light font-sans bg-gray-300'>
+      <div className='text-center'>
+      <h1 className='text-xl font-semibold'>COINSPACE</h1>
+      </div>
+      <Table />
+    </div>
+  )
+}
+
+export default App
